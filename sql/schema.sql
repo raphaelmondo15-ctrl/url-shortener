45ac1a6eef923c642a73a5c66e40ae7d988c6dbf
raphael mondo <raphaelmondo15@gmail.com>
2026-07-09 13:40:50 +0100

    implementing the database logic in the sql file

---
+++ sql/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE links (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(16) NOT NULL UNIQUE,
+    target_url TEXT NOT NULL,
+    created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMPTZ,
+    click_count INTEGER NOT NULL DEFAULT 0 CHECK (click_count >= 0)
+);
+
+CREATE TABLE clicks (
+    id SERIAL PRIMARY KEY,
+    link_id INTEGER NOT NULL REFERENCES links(id) ON DELETE CASCADE,
+    clicked_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    referrer TEXT,
+    user_agent TEXT
+);
+
+CREATE INDEX idx_clicks_link_id ON clicks(link_id);
+
+UPDATE links SET click_count = click_count + 1 WHERE  code = 'your_short_code';
