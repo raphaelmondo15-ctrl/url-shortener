74a4fb32a5fc389d4bf032d4726cdc5803a9605d
raphael mondo <raphaelmondo15@gmail.com>
2026-07-13 12:48:42 +0100

    resolving recursion to avoide the app to generate multiple code and also try to track the uniqueness of the codes

--- src/services/link.service.js
+++ src/services/link.service.js
@@ -2,17 +2,27 @@ import pool from '../config/db.js';
 import { generateShortCode } from './shortCode.service.js';
 
 export async function createShortLink(target_url) {
-    let shortCode = generateShortCode();
-    const result = await pool.query(
-        'SELECT id FROM links WHERE short_code = $1',
-        [shortCode]
-    );
-    if (result.rows.length > 0) {
-        return createShortLink(target_url);
+    let shortCode;
+
+    while (true) {
+        shortCode = generateShortCode();
+
+        const { rows } = await pool.query(
+            'SELECT id FROM links WHERE short_code = $1',
+            [shortCode]
+        );
+
+        if (rows.length === 0) {
+            break;
+        }
     }
-    const newLink = await pool.query(
-        'INSERT INTO links (target_url, short_code) VALUES ($1, $2) RETURNING *',
+
+    const { rows } = await pool.query(
+        `INSERT INTO links (target_url, short_code)
+         VALUES ($1, $2)
+         RETURNING *`,
         [target_url, shortCode]
     );
-    return newLink.rows[0];
+
+    return rows[0];
 }
